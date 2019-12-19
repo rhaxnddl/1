0dfeb3be9f3115e1511adb7266e7410adc90fd62
LeeHyeounGyeoung <58968113+LeeHyeounGyeoung@users.noreply.github.com>
2019-12-19 20:29:27 +0900

    Update Account.java

--- 1907-java/src/f_inheri/Account.java
+++ 1907-java/src/f_inheri/Account.java
@@ -1,7 +1,7 @@
 package f_inheri;
 
 public class Account {
-	// Value object - °ª¸¸ ÀúÀåµÇ¾îÀÖÀ½.
+	// Value object - ê°’ë§Œ ì €ì¥ë˜ì–´ìˆìŒ.
 	private String mName;
 	private String sNo;
 	private long amount;
